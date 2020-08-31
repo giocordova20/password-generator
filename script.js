27bcc1874ca05c092f459061852bde8a96539637
giocode <giocordova@gmail.com>
2020-08-30 23:55:18 -0400

    completed the lowercase generator functions

--- script.js
+++ script.js
@@ -11,18 +11,23 @@ function writePassword() {
 }
 
 // Generate lower case a-z, 97-122 
+// Use Math.random to generate a random decimal and multiply it by 26 to get 1-26 for 
+// the letters of the alphabet a-z. Add 97 to the randome number to get the ascii value
+// for the lower case letter.
 function genRandomLower() {
-  console.log(Math.random());
-
+  // console.log(String.fromCharCode(Math.floor(Math.random() * 26) +97));
+  return String.fromCharCode(Math.floor(Math.random() * 26) +97);
 }
 
+console.log(genRandomLower());
+// console.log(Math.random());
 
-// Print out all the lower case letters from a-z using ascii characters 97-122
-console.log("=======================")
-for (var i=97; i< 122; i++){
-  console.log(String.fromCharCode(i));
-}
-console.log("=======================")
+// // Print out all the lower case letters from a-z using ascii characters 97-122
+// console.log("=======================")
+// for (var i=97; i< 122; i++){
+//   console.log(String.fromCharCode(i));
+// }
+// console.log("=======================")
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
